dc43949d00f4a6ee2bfaa984d22c9107f1dbce99
Setsofia-lab <setsofiaeli@gmail.com>
2022-07-05 12:22:46 +0000

    Updates

---
+++ precache-manifest.6e3cdfe8b7b581ca9efb5205e6487516.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ec38b9491a83f9d1a1d",
+    "url": "/spacebar_app/static/css/main.62e19fc0.chunk.css"
+  },
+  {
+    "revision": "4ec38b9491a83f9d1a1d",
+    "url": "/spacebar_app/static/js/main.4ec38b94.chunk.js"
+  },
+  {
+    "revision": "ae582a3b6c1debc646ca",
+    "url": "/spacebar_app/static/css/1.f78320ba.chunk.css"
+  },
+  {
+    "revision": "ae582a3b6c1debc646ca",
+    "url": "/spacebar_app/static/js/1.ae582a3b.chunk.js"
+  },
+  {
+    "revision": "c99b53ea3d5dd3470817",
+    "url": "/spacebar_app/static/js/runtime~main.c99b53ea.js"
+  },
+  {
+    "revision": "504a27c0e2e122f2ebc0dba4b5e77bf6",
+    "url": "/spacebar_app/static/media/L5.504a27c0.jpg"
+  },
+  {
+    "revision": "832c2d222f5a5e27dc9bdd2ed0afd10f",
+    "url": "/spacebar_app/static/media/wedding.832c2d22.jpg"
+  },
+  {
+    "revision": "81f464e8aa98910ebbf1ac4531a79c7f",
+    "url": "/spacebar_app/static/media/clubs.81f464e8.jpg"
+  },
+  {
+    "revision": "81180ec8c94cfe6f101129454e9f54cc",
+    "url": "/spacebar_app/static/media/L1.81180ec8.jpg"
+  },
+  {
+    "revision": "4eab6fa341699691e5b501d155b631b2",
+    "url": "/spacebar_app/static/media/L2.4eab6fa3.jpg"
+  },
+  {
+    "revision": "6b34e3a54fa6e94bb48c2a0a2a243309",
+    "url": "/spacebar_app/static/media/L3.6b34e3a5.jpg"
+  },
+  {
+    "revision": "504a27c0e2e122f2ebc0dba4b5e77bf6",
+    "url": "/spacebar_app/static/media/L4.504a27c0.jpg"
+  },
+  {
+    "revision": "4f615e21a2384b05d8d238c52dae710d",
+    "url": "/spacebar_app/static/media/background1.4f615e21.jpg"
+  },
+  {
+    "revision": "bcfe94c26a8a485f83ebbaf92f450084",
+    "url": "/spacebar_app/static/media/b2.bcfe94c2.jpg"
+  },
+  {
+    "revision": "451625ddeab8996114069983484b1176",
+    "url": "/spacebar_app/static/media/b3.451625dd.jpg"
+  },
+  {
+    "revision": "a14c732ac08e029acf488aafa2b00721",
+    "url": "/spacebar_app/static/media/b1.a14c732a.jpg"
+  },
+  {
+    "revision": "c2ac6aec130ac83405ee23b0d23a0aeb",
+    "url": "/spacebar_app/index.html"
+  }
+];
